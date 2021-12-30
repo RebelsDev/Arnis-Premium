052fdcb33608b6342507070dc8a071955db8f420
Renzo Damian <renzo.damian.go@gmail.com>
2021-12-30 15:14:04 -0500

    add script

--- pages/_document.js
+++ pages/_document.js
@@ -26,6 +26,7 @@ class MyDocument extends Document {
                     <link rel="manifest" href="/manifest.json" />
                     <meta name="msapplication-TileColor" content="#ffffff" />
                     <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+                    <script type="text/javascript" async="async" src="https://hub.fromdoppler.com/public/dhtrack.js" ></script>
                 </Head>
                 <body>
                     <Main />
