83a769eabcc381f45cb1f6512efa4b7fe7232e11
Renzo Damian <renzo.damian.go@gmail.com>
2022-01-26 10:59:15 -0500

    add facebook pixel

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,23 @@
 import '../styles/globals.scss'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+
+  useEffect(() => {
+    import('react-facebook-pixel')
+      .then((x) => x.default)
+      .then((ReactPixel) => {
+        ReactPixel.init('445187667333106') // facebookPixelId
+        ReactPixel.pageView()
+
+        router.events.on('routeChangeComplete', () => {
+          ReactPixel.pageView()
+        })
+      })
+  }, [router.events])
+
   return <Component {...pageProps} />
 }
 
